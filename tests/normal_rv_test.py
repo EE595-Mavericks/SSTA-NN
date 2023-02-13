9eea36cf1e1a4bc1fafe9874e7078062dc0c873b
star_wyx <576715206@qq.com>
2023-02-12 22:38:57 -0800

    test for normal_rv

---
+++ tests/normal_rv_test.py
@@ -0,0 +1,26 @@
+import math
+import sys
+import numpy as np
+from scipy.stats import skew
+
+if __name__ == "__main__":
+    mean_x = float(sys.argv[1])
+    std_x = math.sqrt(float(sys.argv[2]))
+    mean_y = float(sys.argv[3])
+    std_y = math.sqrt(float(sys.argv[4]))
+
+    x = np.random.normal(mean_x, std_x, size=1000000)
+    y = np.random.normal(mean_y, std_y, size=1000000)
+
+    # Find the maximum of each sample pair
+    max_xy = np.maximum(x, y)
+
+    # Calculate the mean of the maximum
+    mean_max_xy = np.mean(max_xy)
+    variance = np.var(max_xy)
+    # skewness = np.mean(((max_xy - mean_max_xy) / variance) ** 3)
+    skewness = skew(max_xy)
+
+    print("Mean of max(X, Y): {:.5f}".format(mean_max_xy))
+    print("Variance of max(X, Y): {:.5f}".format(variance))
+    print("Skewness of max(X, Y): {:.5f}".format(skewness))
