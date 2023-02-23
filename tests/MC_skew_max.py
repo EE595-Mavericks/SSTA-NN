3a135aae9849caf26171b71768f86991d0483fc3
star_wyx <576715206@qq.com>
2023-02-23 14:34:47 -0800

    add x y three moments

--- tests/MC_skew_max.py
+++ tests/MC_skew_max.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     rows = []
 
     for i in range(100):
-        row = list(range(9))
+        row = list(range(15))
         muX, varX, skeX, x = get_sample()
         muY, varY, skeY, y = get_sample()
         result = subprocess.run(
@@ -32,19 +32,23 @@ if __name__ == "__main__":
             stdout=subprocess.PIPE)
         z = np.array([max(a, b) for (a, b) in zip(x, y)])
         c_output = result.stdout.decode().split(' ')
-        row[0], row[1], row[2] = np.mean(z), np.var(z), skew(z)
-        row[3] = round(float(c_output[0]), 10)
-        row[4] = round(float(c_output[1]), 10)
-        row[5] = round(float(c_output[2]), 10)
-        row[6] = '{:.10%}'.format(fabs((row[0] - row[3]) / row[0]))
-        row[7] = '{:.10%}'.format(fabs((row[1] - row[4]) / row[1]))
-        row[8] = '{:.10%}'.format(fabs((row[2] - row[5]) / row[2]))
+        row[0], row[1], row[2] = np.mean(x), np.var(x), skew(x)
+        row[3], row[4], row[5] = np.mean(y), np.var(y), skew(y)
+        row[6], row[7], row[8] = np.mean(z), np.var(z), skew(z)
+        row[9] = round(float(c_output[0]), 10)
+        row[10] = round(float(c_output[1]), 10)
+        row[11] = round(float(c_output[2]), 10)
+        row[12] = '{:.10%}'.format(fabs((row[6] - row[9]) / row[6]))
+        row[13] = '{:.10%}'.format(fabs((row[7] - row[10]) / row[7]))
+        row[14] = '{:.10%}'.format(fabs((row[8] - row[11]) / row[8]))
         rows.append(row)
 
     with open('MC_skew_max.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
 
-        writer.writerow(['MC mean', 'MC variance', 'MC skewness',
+        writer.writerow(['MC X mean', 'MC X variance', 'MC X skewness',
+                         'MC Y mean', 'MC Y variance', 'MC Y skewness',
+                         'MC mean', 'MC variance', 'MC skewness',
                          'mean', 'variance', 'skewness',
                          'mean error', 'variance error', 'skewness error'])
         for row in rows:
