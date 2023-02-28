523ceecba0a28353a9761325078cf7054f482f07
star_wyx <576715206@qq.com>
2023-02-28 15:00:48 -0800

    Add fz(lower_bound) and fz(upper_bound)

--- tests/MC_skew_max.py
+++ tests/MC_skew_max.py
@@ -35,22 +35,29 @@ if __name__ == "__main__":
         row[0], row[1], row[2] = np.mean(x), np.var(x), skew(x)
         row[3], row[4], row[5] = np.mean(y), np.var(y), skew(y)
         row[6], row[7], row[8] = np.mean(z), np.var(z), skew(z)
-        row[9] = round(float(c_output[0]), 10)
-        row[10] = round(float(c_output[1]), 10)
-        row[11] = round(float(c_output[2]), 10)
+        row[9] = round(float(c_output[2]), 10)
+        row[10] = round(float(c_output[3]), 10)
+        row[11] = round(float(c_output[4]), 10)
         row[12] = '{:.10%}'.format(fabs((row[6] - row[9]) / row[6]))
         row[13] = '{:.10%}'.format(fabs((row[7] - row[10]) / row[7]))
         row[14] = '{:.10%}'.format(fabs((row[8] - row[11]) / row[8]))
+
+        row.insert(0, c_output[1])
+        row.insert(0, c_output[0])
+
         rows.append(row)
 
     with open('MC_skew_max.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
 
-        writer.writerow(['X_mean', 'X_variance', 'X_skewness',
-                         'Y_mean', 'Y_variance', 'Y_skewness',
-                         'MC_Z_mean', 'MC_Z_variance', 'MC_Z_skewness',
-                         'Z_mean', 'Z_variance', 'Z_skewness',
-                         'mean_error', 'variance_error', 'skewness_error'])
+        writer.writerow([
+            'fz(lower_bound)', 'fz(upper_bound)',
+            'X_mean', 'X_variance', 'X_skewness',
+            'Y_mean', 'Y_variance', 'Y_skewness',
+            'MC_Z_mean', 'MC_Z_variance', 'MC_Z_skewness',
+            'Z_mean', 'Z_variance', 'Z_skewness',
+            'mean_error', 'variance_error', 'skewness_error'
+        ])
         for row in rows:
             writer.writerow(row)
 
