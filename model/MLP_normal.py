203b761645578e67574a1bb94b1a2608ed3c411a
James Zhang <zyz991016@outlook.com>
2023-03-26 23:09:56 -0700

    change dataset

--- model/MLP_normal.py
+++ model/MLP_normal.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 # Read CSV file
-data = pd.read_csv("dataset/output.csv")
+data = pd.read_csv("dataset/normal_rv_dataset.csv")
 
 # Split data into input and output
 x = data.iloc[:, :4].values
