181f6ecff3c5fb957f69fd368a633336e03d1607
James Zhang <zyz991016@outlook.com>
2023-04-14 19:02:09 -0700

    1d-2d

--- model/MLP_normal_mean.py
+++ model/MLP_normal_mean.py
@@ -98,8 +98,8 @@ if __name__ == "__main__":
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
     x_train = torch.tensor(x_train, dtype=torch.float32)
     x_test = torch.tensor(x_test, dtype=torch.float32)
-    y_train = torch.tensor(y_train, dtype=torch.float32)
-    y_test = torch.tensor(y_test, dtype=torch.float32)
+    y_train = torch.tensor(y_train, dtype=torch.float32).unsqueeze(1)
+    y_test = torch.tensor(y_test, dtype=torch.float32).unsqueeze(1)
 
     # Set device to GPU if available, else CPU
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
